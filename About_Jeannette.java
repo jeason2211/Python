81accd074fca1495d91b5f920336c8943ef90df9
JEason <71575998+jeason2211@users.noreply.github.com>
2023-10-11 08:32:30 -0400

    Add files via upload

---
+++ About_Jeannette.java
@@ -0,0 +1,7 @@
+public class About_Jeannette {
+  public static void main(String[] args) {
+    System.out.println("Jeannette Eason");
+    System.out.println("Information Systems");
+    System.out.println("Huntington National Bank");
+  }
+}
